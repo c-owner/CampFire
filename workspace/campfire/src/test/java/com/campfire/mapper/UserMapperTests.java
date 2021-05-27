3d0a5350be29eb9ea825485a93bb6e08a5007290
heekng <rhkd1769@gmail.com>
2021-05-27 18:20:45 +0900

    버그지만 커밋했어요

--- workspace/campfire/src/test/java/com/campfire/mapper/UserMapperTests.java
+++ workspace/campfire/src/test/java/com/campfire/mapper/UserMapperTests.java
@@ -4,21 +4,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.campfire.domain.UserVO;
-import com.campfire.mapper.UserMapper;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class UserMapperTests {
 	@Setter(onMethod_ = @Autowired)
 	private UserMapper mapper;
 	
+	/*
 	@Test
 	public void testInsert() {
 		UserVO user = new UserVO();
@@ -28,6 +27,11 @@ public class UserMapperTests {
 		
 		log.info(mapper.insert(user));
 	}
+	*/
 	
+	@Test
+	public void testSelectId() {
+		log.info(mapper.selectId("tester"));
+	}
 	
 }
