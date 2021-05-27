11d2983f2e4afd9d0919de33c3d2fecd9b27e26d
heekng <rhkd1769@gmail.com>
2021-05-27 17:22:56 +0900

    make userMapperTests

---
+++ workspace/campfire/src/test/java/campfire/UserMapperTests.java
@@ -0,0 +1,22 @@
+package campfire;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.campfire.mapper.UserMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class UserMapperTests {
+	@Setter(onMethod_ = @Autowired)
+	private UserMapper mapper;
+	
+	
+	
+}
