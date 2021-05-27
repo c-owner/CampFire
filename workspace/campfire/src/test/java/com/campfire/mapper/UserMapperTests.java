0488caffeda98e8a1221918a64f3792437e0d20b
Eight-Corner <corner3499@gmail.com>
2021-05-27 18:26:55 +0900

    본프로젝트 Tests

--- workspace/campfire/src/test/java/com/campfire/mapper/UserMapperTests.java
+++ workspace/campfire/src/test/java/com/campfire/mapper/UserMapperTests.java
@@ -1,37 +1,45 @@
 package com.campfire.mapper;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import org.springframework.test.context.junit4.SpringRunner;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class UserMapperTests {
+	
 	@Setter(onMethod_ = @Autowired)
 	private UserMapper mapper;
 	
-	/*
-	@Test
-	public void testInsert() {
-		UserVO user = new UserVO();
-		user.setUserEmail("test@test.com");
-		user.setUserId("tester");
-		user.setUserPw("testerPw");
-		
-		log.info(mapper.insert(user));
-	}
-	*/
+//	@Test
+//	public void testSelectPw() {
+//		log.info(mapper.selectPw("tester"));
+//	}
 	
-	@Test
-	public void testSelectId() {
-		log.info(mapper.selectId("tester"));
-	}
+//	@Test
+//	public void testSelectEmail() {
+//		log.info(mapper.selectEmail("test@test.com"));
+//	}
+	
+//	@Test
+//	public void testSelectId() {
+//		log.info(mapper.selectId("tester"));
+//	}
+	
+//	@Test
+//	public void testInsert() {
+//		UserVO user = new UserVO();
+//		user.setUserEmail("test1@test.com");
+//		user.setUserId("tester1");
+//		user.setUserPw("testerPw1");
+//		
+//		log.info(mapper.insert(user));
+//	}
+//	
 	
 }
