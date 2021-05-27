7de1cdb1dfb5b616152370bac68eb3d6af628c5c
mamulee <ghdah7@gmail.com>
2021-05-27 17:22:39 +0900

    signup signin

--- workspace/campfire/src/main/java/com/campfire/service/UserService.java
+++ workspace/campfire/src/main/java/com/campfire/service/UserService.java
@@ -1,5 +1,10 @@
 package com.campfire.service;
 
-public interface UserService {
+import com.campfire.domain.UserVO;
 
+public interface UserService {
+	public boolean checkId(String userId);
+	public boolean checkEmail(String userEmail);
+	public boolean register(UserVO user);
+	public boolean login(String userId, String userPw);
 }
