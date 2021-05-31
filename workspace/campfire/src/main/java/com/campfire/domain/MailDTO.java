551583f854cd372589179454322b72071d806af7
heekng <rhkd1769@gmail.com>
2021-05-31 15:25:09 +0900

    add MailDTO -> boolean result = new MailDTO(받는 이메일, 제목, 내용).sendMail(); 로 사용 result 는 중간 에러 여부

---
+++ workspace/campfire/src/main/java/com/campfire/domain/MailDTO.java
@@ -0,0 +1,61 @@
+package com.campfire.domain;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import lombok.Data;
+import lombok.extern.log4j.Log4j;
+
+@Data
+@Log4j
+public class MailDTO {
+	private String receiver;
+	private String title;
+	private String content;
+	
+	public MailDTO(String receiver, String title, String content) {
+		this.receiver = receiver;
+		this.title = title;
+		this.content = content;
+	}
+	
+	public boolean sendmail() {
+		final String user = "구글이메일"; //발신자의 이메일 아이디를 입력
+		final String password = "비밀번호"; //발신자 이메일의 패스워드를 입력
+		
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", 465);
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.ssl.enable", "true");
+		prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		
+		Session session = Session.getDefaultInstance(prop, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(user, password);
+			}
+		});
+		
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(user));
+			
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(this.receiver));
+			
+			message.setSubject(this.title);
+			message.setText(this.content);
+			
+			Transport.send(message);
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+		
+	}
+}
