fd0f012174303947600ba6d80b70c898f13b2a05
heekng <rhkd1769@gmail.com>
2021-06-04 15:35:13 +0900

    reviewView controller 수정

--- workspace/campfire/src/main/java/com/campfire/controller/ReviewController.java
+++ workspace/campfire/src/main/java/com/campfire/controller/ReviewController.java
@@ -3,10 +3,13 @@ package com.campfire.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.campfire.domain.Criteria;
 import com.campfire.domain.PageDTO;
+import com.campfire.domain.ReviewBoardVO;
 import com.campfire.service.ReviewBoardService;
 
 import lombok.AllArgsConstructor;
@@ -36,6 +39,10 @@ public class ReviewController {
 	
 	
 	@GetMapping(value="/reviewView")
-	public void reviewView() {}
+	public void reviewView(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
+		log.info("reviewView....." + bno);
+		log.info("reviewView....." + cri);
+		model.addAttribute("review", service.view(bno));
+	}
 	
 }
