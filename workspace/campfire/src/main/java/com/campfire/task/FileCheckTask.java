958d50f4a71f838bb72987120839b1242e74331a
junnnn <darktraces@nate.com>
2021-06-07 22:24:35 +0900

    게시글 수정/삭제

---
+++ workspace/campfire/src/main/java/com/campfire/task/FileCheckTask.java
@@ -0,0 +1,63 @@
+package com.campfire.task;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.campfire.domain.freeBoard.FreeBoardAttachVO;
+import com.campfire.mapper.FreeBoardAttachMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Component
+@Log4j
+public class FileCheckTask {
+	
+	@Setter(onMethod_ = @Autowired)
+	private FreeBoardAttachMapper attachMapper;
+	
+	@Scheduled(cron="0 * * * * *")
+	public void checkFiles() throws Exception {
+		log.warn("-------------------------------");
+		log.warn("file Check tast run............");
+		log.warn("-------------------------------");
+		
+		List<FreeBoardAttachVO> fileList = attachMapper.getOldFiles();
+		
+		List<Path> fileListPaths = fileList.stream().map(vo -> 
+			Paths.get("C:\\upload", vo.getUploadPath(), vo.getUuid() + "_" + vo.getFileName()))
+				.collect(Collectors.toList());
+		
+		fileList.stream().filter(vo -> vo.isFileType()).map(vo ->
+			Paths.get("C:\\upload", vo.getUploadPath(), "s_" + vo.getUuid() + "_" + vo.getFileName()))
+		.forEach(p -> fileListPaths.add(p));
+		
+		fileListPaths.forEach(log::warn);
+		
+		File targetDir = Paths.get("C:\\upload", getFolderYesterDay()).toFile();
+		
+		File[] removeFiles = targetDir.listFiles(file -> !fileListPaths.contains(file.toPath()));
+		
+		for (File file : removeFiles) {
+			log.warn(file.getPath() + " deleted");
+			file.delete();
+		}
+	}
+	
+	private String getFolderYesterDay() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -1);
+		
+		return sdf.format(cal.getTime()).replace("-", File.separator);
+	}
+}
--- workspace/campfire/src/main/webapp/WEB-INF/views/free/freeList.jsp
+++ workspace/campfire/src/main/webapp/WEB-INF/views/free/freeList.jsp
@@ -9,7 +9,7 @@
       <meta name="description" content="" />
       <meta name="keywords" content="" />
       <link rel="stylesheet" href="/resources/assets/css/main.css" />
-      <link rel="shortcut icon" type="image/x-icon" href="/resources/images/title-icon.png">
+      <link rel="shortcut icon" type="image/x-icon" href="/resources/images/icon/title-icon.png">
       <style>
       .table-wrapper{
         width: 80%;
